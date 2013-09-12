af7a587479654124b1ad2ac5cc62e6d456758450
Kostas K <mpokwsths@gmail.com>
2013-09-12 23:30:37 +0300

    ARM: keep __my_cpu_offset consistent with generic one
    Commit 14318efb(ARM: 7587/1: implement optimized percpu variable access)
    introduces arm's __my_cpu_offset to optimize percpu vaiable access,
    which really works well on hackbench, but will cause __my_cpu_offset
    to return garbage value before it is initialized in cpu_init() called
    by setup_arch, so accessing percpu variable before setup_arch may cause
    kernel hang. But generic __my_cpu_offset always returns zero before
    percpu area is brought up, and won't hang kernel.
    
    So the patch tries to clear __my_cpu_offset on boot CPU early
    to avoid boot hang.
    
    At least now percpu variable is accessed by lockdep before
    setup_arch(), and enabling CONFIG_LOCK_STAT or CONFIG_DEBUG_LOCKDEP
    can trigger kernel hang.
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Rob Herring <rob.herring@calxeda.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Nicolas Pitre <nico@linaro.org>
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -366,6 +366,13 @@ static void __init setup_processor(void)
 
 	cpu_name = list->cpu_name;
 
+	/*
+	 * clear __my_cpu_offset on boot CPU to avoid hang caused by
+	 * using percpu variable early, for example, lockdep will
+	 * access percpu variable inside lock_release
+	 */
+	set_my_cpu_offset(0);
+
 #ifdef MULTI_CPU
 	processor = *list->proc;
 #endif
@@ -394,7 +401,6 @@ static void __init setup_processor(void)
 
 	cacheid_init();
 	cpu_proc_init();
-	set_my_cpu_offset(0);
 }
 
 /*
